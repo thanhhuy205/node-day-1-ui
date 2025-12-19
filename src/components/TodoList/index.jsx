f502daf1ac60b43f89cc4bc31236076d64c45511
Phạm Thanh Huy <huyp7922@gmail.com>
2025-12-19 17:09:25 +0700

    up

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./todolist.module.css";
 
-const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export default function TodoList() {
   const [tasks, setTasks] = useState([]);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/day-1-ui/", // Thay "day-1-ui" bằng tên repo GitHub của bạn
+});
